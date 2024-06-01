c8b433e67f560a083f2f24827321052e98fa98e9
babaj301 <babajideoluwaferanmi301@gmail.com>
2024-06-01 19:39:01 +0100

    Promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,11 +1,40 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
-import { loadProducts } from "../data/products.js";
+import { loadProducts, loadProductsFetch } from "../data/products.js";
 
 // import "../data/cart-class.js";
 // import "../data/backend-practice.js";
 
-loadProducts(() => {
-  renderOrderSummary();
-  renderPaymentSummary();
-});
+async function loadPage() {
+  console.log("load page");
+  await loadProductsFetch();
+
+  await new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  });
+
+  return "value2";
+}
+
+// loadPage().then((value) => {
+//   console.log("next step");
+
+//   loadProducts()
+//   console.log(value);
+// });
+
+// new Promise((resolve) => {
+//   loadProducts(() => {
+//     resolve();
+//   });
+// }).then(() => {
+//   renderOrderSummary();
+//   renderPaymentSummary();
+// });
+
+// loadProducts(() => {
+//   renderOrderSummary();
+//   renderPaymentSummary();
+// });
